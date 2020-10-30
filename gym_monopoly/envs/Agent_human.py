86b0d949265270d0615922704324f7ecd67ed70b
Andrew Kim <andrewkim260@gmail.com>
2020-10-29 19:42:54 -0700

    Minor updates to code blocks in action_helper

--- gym_monopoly/envs/Agent_human.py
+++ gym_monopoly/envs/Agent_human.py
@@ -7,28 +7,6 @@ class Agent_human:
     def set_action_type(self):
         pass
 
-    def print_properties(self):
-        print(f"\n{player.symbol} Properties: ")
-        tmp = sorted(player.property_in_use + player.property_in_mort)
-        for i in tmp:
-            if player.check_property_index_for_houses(i):
-
-                if i in player.property_in_mort:
-                    print(f'{self.board[i][3]} - {self.board[i][2]} [{i}] - In Mortgage' + Style.RESET_ALL)
-                else:
-                    print(f'{self.board[i][3]} - {self.board[i][2]} [{i}]' + Style.RESET_ALL)
-            else:
-                if i in player.property_in_mort:
-                    if i == 12 or i == 28:
-                        print(f'Util: {self.board[i][2]} [{i}] - In Mortgage')
-                    else:
-                        print(f'Rail: {self.board[i][2]} [{i}] - In Mortgage')
-                else:
-                    if i == 12 or i == 28:
-                        print(f'Util: {self.board[i][2]} [{i}]')
-                    else:
-                        print(f'Rail: {self.board[i][2]} [{i}]')
-
     def get_action(self, obs):
         NUMBER_OF_ACTIONS = 66
         print("-------------------------------")
